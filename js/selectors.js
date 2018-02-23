3d581c48951361e847bf0fcbe296bf2920a9c746
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-23 21:07:29 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,7 +1,12 @@
 // declare your functions here...
-$('img')
-$('.pics')
-$('#baby-ninja')
-$('ul li')
-$('div img:first-child')
-$("img[alt= 'the beatles making faces']")
+function paragraphSelector() {
+  $('p')
+}
+
+
+// $('img')
+// $('.pics')
+// $('#baby-ninja')
+// $('ul li')
+// $('div img:first-child')
+// $("img[alt= 'the beatles making faces']")
